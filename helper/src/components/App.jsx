4a29acfe06ee6836bb681aaadae601e72e48033b
allentsangdev <allentsang04@gmail.com>
2023-03-30 03:40:16 -0400

    update home page and routing

--- helper/src/components/App.jsx
+++ helper/src/components/App.jsx
@@ -1,13 +1,20 @@
 import "./app.css"
+import HomePage from "./HomePage";
 import Navbar from "./Navbar"
 import QuestionCard from "./QuestionCard"
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
     return(
-        <>
+        
+        <BrowserRouter>
             <Navbar/>
-            <QuestionCard/>
-        </>
+            <Routes>
+                <Route path='/' element={<HomePage/>} />
+                <Route path='/quiz' element={<QuestionCard/>} exact />
+            </Routes>
+        </BrowserRouter>
+        
     )
 }
 
---
+++ helper/src/components/HomePage.jsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+
+function HomePage() {
+
+    return(
+        
+        <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url('https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80')`,
+      }}
+    >
+      
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container>
+        <Grid item md={6}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              Want to ace your dirving exam?
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Try our quizlet now!
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+
+        
+    )
+}
+
+export default HomePage
+
+
+
+
+
+
