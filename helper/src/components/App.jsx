6a5883bc849d338d310dc47953f4e0e679c4f857
allentsangdev <allentsang04@gmail.com>
2023-04-02 06:16:47 -0400

    update font style

--- helper/src/components/App.jsx
+++ helper/src/components/App.jsx
@@ -4,10 +4,20 @@ import Navbar from "./Navbar"
 import QuestionCard from "./QuestionCard"
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Footer from "./Footer";
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+
+
+const theme = createTheme({
+    typography: {
+      fontFamily: ['Jura', 'sans-serif'].join(','),
+    },
+  });
 
 function App() {
     return(
-        
+        <ThemeProvider theme={theme}>
+
         <BrowserRouter>
             <Navbar/>
             <Routes>
@@ -16,6 +26,8 @@ function App() {
             </Routes>
             <Footer/>
         </BrowserRouter>
+        </ThemeProvider>
+
         
     )
 }
