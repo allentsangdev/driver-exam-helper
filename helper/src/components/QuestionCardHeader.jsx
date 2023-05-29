2ff65e4e53064161d3b5eca53bea5ffb9aca6c96
allentsangdev <allentsang04@gmail.com>
2023-05-29 01:19:32 -0400

    fix react-checkmark installation

--- helper/src/components/QuestionCardHeader.jsx
+++ helper/src/components/QuestionCardHeader.jsx
@@ -16,7 +16,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useFirestore } from '../contexts/FirestoreContext';
-import CloseIcon from '@mui/icons-material/Close';
 import { Checkmark } from 'react-checkmark'
 
 export default function QuestionCardHeader() {
